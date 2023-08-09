38680e2de28b0c3125d60d7a55e8549c6b6f5c04
Dmitrijs Osipovs <dmitrijsosipov@gmail.com>
2023-08-09 17:13:06 +0300

    script fix

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -4,11 +4,14 @@ import { PrismicPreview } from "@prismicio/next";
 
 import { repositoryName } from "@/prismicio";
 
+import Script from 'next/script'
+
 export default function App({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <PrismicPreview repositoryName={repositoryName} />
+      <Script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/a757ef95be3f39fec6dff6f5/script.js"></Script>
     </>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,11 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Script from 'next/script'
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <Script id="cookieyes" type="text/javascript" src="https://cdn-cookieyes.com/client_data/a757ef95be3f39fec6dff6f5/script.js"></Script>
         <link
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap"
           rel="stylesheet"
